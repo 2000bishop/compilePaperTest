135816971992daa06c3347035d1f209bdc557e15
townsend.bishop <townsend.bishop@vashonsd.org>
2021-10-06 10:48:17 -0700

    Initial commit

---
+++ src/org/vashonsd/Main.java
@@ -0,0 +1,12 @@
+package org.vashonsd;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.print(squareOf(3));
+    }
+
+        public static int squareOf ( int num){
+    return(num * num);
+    }
+}
